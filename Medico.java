7b8bfdfaa3eec8e00e5dc9a6b73e8cbaecb8fc51
ianzinho4009 <128252672+ianzinho4009@users.noreply.github.com>
2023-03-18 15:41:46 -0300

    Update Medico.java

--- Medico.java
+++ Medico.java
@@ -1,1 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.meprogamacao;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ian
+ */
+public class Medicos {
+    private String crm;
+    private String nome;
+    private Date datanascimento;
+    private Date datacadastro;
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDatanascimento() {
+        return datanascimento;
+    }
+
+    public void setDatanascimento(Date datanascimento) {
+        this.datanascimento = datanascimento;
+    }
+
+    public Date getDatacadastro() {
+        return datacadastro;
+    }
+
+    public void setDatacadastro(Date datacadastro) {
+        this.datacadastro = datacadastro;
+    }
+    
+        
+    }
+    
+    
+    
+   
+
+    
+
 
